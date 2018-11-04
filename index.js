39eb6d9bbf66e4e8054501fd1628a6bbced41653
K <kamiyaowl@gmail.com>
2018-11-04 16:42:55 +0900

    Remo APIからセンサ情報を取得

--- index.js
+++ index.js
@@ -5,17 +5,31 @@ const REMO_API_KEY = process.env.REMO_API_KEY || config.REMO_API_KEY;
 
 exports.update = (event, callback) => {
     request.get({
-        url: 'https://api.nature.global/1/users/me',
+        url: 'https://api.nature.global/1/devices',
         headers: {
             'Authorization': `Bearer ${REMO_API_KEY}`,
         },
      }, (err, res, body) => {
         if(err || res.statusCode !== 200) {
-            console.error(err, res);
+            console.error(`Remo API Error err:${err}, res:${res}`);
             callback();
             return;
         }
-        const data = JSON.parse(body);
-        callback(data);
+        const datas = JSON.parse(body);
+        datas.forEach(data => {
+            const name = data.name;
+            const te_offset = data.temperature_offset;
+            const hu_offset = data.humidity_offset;
+            const newest_events = data.newest_events;
+            if (!newest_events) {
+                console.error('newest_events not found');
+                callback();
+                return;
+            }
+            const te = newest_events.te;
+            const hu = newest_events.hu;
+            const il = newest_events.il;
+            callback(data);
+        });
     });
 };
