bcd13dce7361162da70241b88105f6e54ab7f370
K <kamiyaowl@gmail.com>
2018-11-04 17:08:25 +0900

    Ambientへのデータ送信を実装

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const request = require('request');
 const config = require('config');
+const ambient = require('ambient-lib');
 
 const REMO_API_KEY = process.env.REMO_API_KEY || config.REMO_API_KEY;
+const REMO_DEVICE_ID = process.env.REMO_DEVICE_ID || config.REMO_DEVICE_ID || 0;
+const AMBIENT_CHANNEL_ID = process.env.AMBIENT_CHANNEL_ID || config.AMBIENT_CHANNEL_ID;
+const AMBIENT_WRITE_KEY = process.env.AMBIENT_WRITE_KEY || config.AMBIENT_WRITE_KEY;
 
 exports.update = (event, callback) => {
     request.get({
@@ -15,21 +19,45 @@ exports.update = (event, callback) => {
             callback();
             return;
         }
+        // prepare data        
+        const send_arr = [];
         const datas = JSON.parse(body);
-        datas.forEach(data => {
-            const name = data.name;
-            const te_offset = data.temperature_offset;
-            const hu_offset = data.humidity_offset;
-            const newest_events = data.newest_events;
-            if (!newest_events) {
-                console.error('newest_events not found');
+        if (datas.length < REMO_DEVICE_ID) {
+            console.error('datas.length < REMO_DEVICE_ID');
+            callback();
+            return;
+        }
+        const data = datas[REMO_DEVICE_ID];
+        const name = data.name;
+        const te_offset = data.temperature_offset;
+        const hu_offset = data.humidity_offset;
+        const newest_events = data.newest_events;
+        if (!newest_events) {
+            console.error('newest_events not found');
+            callback();
+            return;
+        }
+        const te = newest_events.te;
+        const hu = newest_events.hu;
+        const il = newest_events.il;
+        if (te) {
+            send_arr.push({created: te.created_at, d1: te.val});
+        }
+        if (hu) {
+            send_arr.push({created: hu.created_at, d2: hu.val});
+        }
+        if (il) {
+            send_arr.push({created: il.created_at, d3: il.val});
+        }
+        // send to ambient
+        ambient.connect(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY);
+        ambient.send(send_arr, (err2, res2, body2) => {
+            if (err2 || res2.statusCode != 200) {
+                console.error(`Ambient API Error err:${err2}, res:${res2}`);
                 callback();
                 return;
             }
-            const te = newest_events.te;
-            const hu = newest_events.hu;
-            const il = newest_events.il;
-            callback(data);
+            callback();
         });
     });
 };
